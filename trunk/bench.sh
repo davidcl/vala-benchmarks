44011264e2f40a2b8ec290e40100847112df4430
c.david86 <c.david86@76db0cc2-5938-0410-a305-7156983723fc>
2007-08-29 15:51:51 +0000

    Complete Makefile organisation

---
+++ trunk/bench.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+# Init
+echo "Bench Name;Mono;Vala;plain-C" > bench.csv
+
+# Loop
+for dir in $*
+do	
+	echo $dir';'$(cat `find ./$dir/ |grep csv` |tr '\n' ';' |sed 's/,$//') >> bench.csv	
+done;
