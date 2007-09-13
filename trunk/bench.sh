1bfc594ee8943323e4047f2d0ae10617747a7e26
c.david86 <c.david86@76db0cc2-5938-0410-a305-7156983723fc>
2007-09-13 16:16:28 +0000

    * Normalize benchs time duration * Adding auto-generated graphs (with ploticus)

--- trunk/bench.sh
+++ trunk/bench.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 # Init
-echo "Bench Name;Mono;Vala;plain-C" > bench-time.csv
-echo "Bench Name;Mono;Vala;plain-C" > bench-size.csv
+echo "BenchName;Mono;Vala;plain-C" > bench-time.csv
+echo "BenchName;Mono;Vala;plain-C" > bench-size.csv
 
 # Loop
 for dir in $*
@@ -12,6 +12,20 @@ do
 done;
 
 # (warning: just a note)
-# to convert to html and Google Wiki syntax
-# sed 's|;|</td><td>|g;s|^\(.*\)$|<tr><td>\1</td></tr>|' data.csv > out.html
-# sed 's/;/||/g;s/^\(.*\)$/||\1||/' data.csv > out.html
+# to convert to html , Google Wiki syntax and Tab separated values
+# sed 's|;|</td><td>|g;s|^\(.*\)$|<tr><td>\1</td></tr>|' data.csv
+# sed 's/;/||/g;s/^\(.*\)$/||\1||/' data.csv
+# sed 's/;/\t/g;s/^\(.*\)$/\t\1\t/' data.csv
+
+# Generating charts
+#
+cp ploticus-time.tmpl template
+sed 's/;/\t/g;s/^\(.*\)$/\1\t/' bench-time.csv >> template
+ploticus -png -o bench-time.png template
+rm template
+#
+cp ploticus-size.tmpl template
+sed 's/;/\t/g;s/^\(.*\)$/\1\t/' bench-size.csv >> template
+ploticus -png -o bench-size.png template
+rm template
+#
